376e2d245315ff39f8e43649e5685f4ba940d279
Ny4b0r0n <nguyenquoccuong290900@gmail.com>
2021-07-06 21:52:23 +0700

    tr37

--- 1811190667_NguyenQuocCuong_BigSchool01/Models/ApplicationDbContext.cs
+++ 1811190667_NguyenQuocCuong_BigSchool01/Models/ApplicationDbContext.cs
@@ -11,12 +11,21 @@ namespace _1811190667_NguyenQuocCuong_BigSchool01.Models
     {
         public DbSet<Course> Courses { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
         public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Course)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
