26024036b043a88a765fb7bc2ece55cff7937061
Ny4b0r0n <nguyenquoccuong290900@gmail.com>
2021-07-08 21:38:19 +0700

    tr59

--- 1811190667_NguyenQuocCuong_BigSchool01/ViewModels/CourseViewModel.cs
+++ 1811190667_NguyenQuocCuong_BigSchool01/ViewModels/CourseViewModel.cs
@@ -9,7 +9,7 @@ namespace _1811190667_NguyenQuocCuong_BigSchool01.ViewModels
 {
     public class CourseViewModel
     {
-
+        public int Id { get; set; }
         [Required(ErrorMessage = " Place is Required")]
         public string Place { get; set; }
         [Required(ErrorMessage = " Date is Required")]
@@ -21,6 +21,11 @@ namespace _1811190667_NguyenQuocCuong_BigSchool01.ViewModels
         [Required(ErrorMessage = " Category is Required")]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
+        public string Heading { get; set; }
+        public string Action
+        {
+            get { return (Id != 0) ? "Update" : "Create"; }
+        }
         public DateTime GetDateTime()
         {
             return DateTime.Parse(string.Format("{0} {1}", Date, Time));
